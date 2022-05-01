e32014dc6486e7cdaeb5f2bbfa62fb6ad44b6991
Brianna Gaines <briannagaines31@gmail.com>
2022-04-30 18:53:35 -0600

    init commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+import Analysis from "./components/Analysis";
+import Menu from "./components/Analysis";
+import Metrics from "./components/Metrics";
+import Ratings from "./components/Ratings";
+import Reviews from "./components/Reviews";
+import Visitors from "./components/Visitors";
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Visitors.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Visitors = (props) => {
+    return
+}
+
+export default Visitors;
